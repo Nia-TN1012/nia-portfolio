cc67a2d180c88e87ed57126836d888ccc5c3c259
Nia Tomonaka <niatn1012@Nia-Sequoia.local>
2025-08-17 16:31:41 +0900

    fix

--- src/pages/AboutPage.vue
+++ src/pages/AboutPage.vue
@@ -28,13 +28,13 @@ import SnsButtonComponent from "../components/SnsButton.vue";
             <tr>
               <th><i class="fas fa-landmark"></i> Speciality</th>
               <td>
-                フロントエンドやサーバーサイドでの開発、データベースの設計・チューニング、インフラ（AWSやGCPなどのクラウドを利用）の構成・運用
-                / ブロックチェーン技術周りの研究・コントラクト開発
+                ITエンジニア（バックエンド、フロントエンド、データベース、インフラ（クラウド））、
+                ブロックチェーンエンジニア、プログラマー
               </td>
             </tr>
             <tr>
               <th><i class="far fa-play-circle"></i> Hobby</th>
-              <td>写真撮影、音ゲー、料理</td>
+              <td>写真撮影、旅行、料理、アニメ鑑賞</td>
             </tr>
             <tr>
               <th><i class="fas fa-link"></i> SNS / Links</th>
